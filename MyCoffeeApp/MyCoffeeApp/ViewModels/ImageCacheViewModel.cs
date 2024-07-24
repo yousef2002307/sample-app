d950887b7495e7d6ade9e7dd7449d80d0f5f6aa9
yousef2002307 <85695400+yousef2002307@users.noreply.github.com>
2024-07-24 19:43:23 +0300

    First Commit

---
+++ MyCoffeeApp/MyCoffeeApp/ViewModels/ImageCacheViewModel.cs
@@ -0,0 +1,48 @@
+﻿using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Command = Xamarin.Forms.Command;
+
+namespace MyCoffeeApp.ViewModels
+{
+    public  class ImageCacheViewModel : ViewModelBase
+    {
+        public UriImageSource Image { get; set; } =
+            new UriImageSource
+            {
+                Uri = new Uri("https://images.wsdot.wa.gov/sw/005vc00032.jpg"),
+                CachingEnabled = true,
+                CacheValidity = TimeSpan.FromMinutes(1)
+            };
+
+        public Command RefreshCommand { get; }
+
+        public ImageCacheViewModel()
+        {
+            RefreshCommand = new Command(() =>
+            {
+                Image = new UriImageSource
+                {
+                    Uri = new Uri("https://images.wsdot.wa.gov/sw/005vc00032.jpg"),
+                    CachingEnabled = true,
+                    CacheValidity = TimeSpan.FromMinutes(1)
+                };
+                OnPropertyChanged(nameof(Image));
+            });
+
+            RefreshLongCommand = new AsyncCommand(async () =>
+            {
+                IsBusy = true;
+                await Task.Delay(5000);
+                IsBusy = false;
+            });
+
+        }
+
+        public AsyncCommand RefreshLongCommand { get; }
+
+    }
+}
